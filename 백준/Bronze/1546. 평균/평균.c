4cf12d389126352bb07050b91848500b3d384750
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-06-19 00:57:30 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.c
+++ 백준/Bronze/1546. 평균/평균.c
@@ -6,15 +6,12 @@ int main() {
     float num[n], m=0, sum=0;
     for (int i=0;i!=n;i++){
         scanf("%f",&num[i]);
+        sum+=num[i];
         if (m<num[i]){
-            sum+=m;
             m=num[i];
         }
-        else{
-            sum+=num[i];
-        }
     }
-    printf("%.2f",((((sum+m)/m)*100))/n);
+    printf("%.2f",((((sum)/m)*100))/n);
 
     return 0;
 }
