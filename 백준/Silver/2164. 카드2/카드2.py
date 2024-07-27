903c82e46dde3cb4069b5b71af27a95762535ff2
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-07-27 14:54:41 +0900

    [Silver IV] Title: 카드2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,11 +4,8 @@ if (num==1):
     print(1)
 else:
     for i in range(20):
-        m=int(2**(i+1))
+        m=int(2**(i-1))
         n=int(2**i)
-        if (num>n and num<=m):
+        if (num>m and num<=n):
             break
-    if (num%(2**i)!=0):
-        print((num%(2**i))*2+(2**i)*(num//(2**i)-1))
-    else:
-        print((num%(2**i))*2+(2**i)*(num//(2**i)))
+    print(2**i-(2**i-num)*2)
