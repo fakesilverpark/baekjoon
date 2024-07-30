be00bdd83e7fc5a6a6d6c0bbbf9a2cec89c70a85
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-07-30 16:55:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2900 ms, Memory: 61116 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,10 @@
+n=int(input())
+points=[]
+for i in range(n):
+    apoint=input().split()
+    apoint[0]=int(apoint[0])
+    apoint[1]=int(apoint[1])
+    points.append(apoint)
+points.sort(key=lambda x: (x[0], x[1]))
+for i in points:
+    print(i[0],i[1])
