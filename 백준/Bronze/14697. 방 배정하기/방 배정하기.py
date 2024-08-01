f4fb5cd19853ab2cf6d1e7375c407d0bea4dd2e5
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-08-02 00:17:58 +0900

    [Bronze II] Title: 방 배정하기, Time: 132 ms, Memory: 109240 KB, Score: 22 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -0,0 +1,29 @@
+a,b,c,r=map(int,input().split())
+cnt=0
+if (r%a==0):
+    print(1)
+elif (r%b==0):
+    print(1)
+elif (r%c==0):
+    print(1)
+else:
+    for ai in range (1,r//a+1):
+        if (cnt!=0):
+            break
+        aroom=r
+        aroom-=ai*a
+        for bi in range(1,aroom//b+1):
+            if (cnt!=0):
+                break
+            broom=aroom
+            broom-=bi*b
+            for ci in range(1,broom//c+1):
+                croom=broom
+                croom-=ci*c
+                if (croom==0):
+                    cnt+=1
+                    break
+    if (cnt==0):
+        print(0)
+    else:
+        print(1)
