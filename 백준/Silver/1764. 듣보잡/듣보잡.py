b3b16f890953284161f12bd01d7747a2b319386c
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-08-04 22:01:17 +0900

    [Silver IV] Title: 듣보잡, Time: 2512 ms, Memory: 43924 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,17 @@
+n,m=map(int,input().split())
+
+nSet=set()
+for _ in range(n):
+    nSet.add(input())
+
+mSet=set()
+for _ in range(m):
+    mSet.add(input())
+
+resultSet=list(nSet&mSet)
+resultSet.sort()
+
+print(len(resultSet))
+
+for i in resultSet:
+    print(i)
