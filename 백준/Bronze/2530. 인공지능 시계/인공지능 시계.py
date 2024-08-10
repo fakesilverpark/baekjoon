084ccf4fb3158ff2ef9a22eaeab05e0dda3d6198
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-08-11 00:46:56 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,8 +1,10 @@
 a,b,c=map(int,input().split())
 n=int(input())
+
 time=a*3600+b*60+c+n
-print(time//3600-24*((time//3600)//24),end=" ")
+
+t=time//3600
+print(t-24*(t//24),end=" ")
 time%=3600
 print(time//60,end=" ")
-time%=60
-print(time)
+print(time%60)
