73f62186dce4ef621609343c4d3fef2bb6e279a5
eunbb <167179789+fakesilverpark@users.noreply.github.com>
2024-08-31 21:26:24 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 110856 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,24 +1,14 @@
-from collections import deque
-
-def checkcheck_b(arr):
+def checkcheck(arr, check):
     cnt = 0
-    check = ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
-    for i in range(64):
-        if (check[i] != arr[i]):
-            cnt += 1
-    return cnt
-
-def checkcheck_w(arr):
-    cnt = 0
-    check = ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
     for i in range(64):
         if (check[i] != arr[i]):
             cnt += 1
     return cnt
 
 def checking(arr):
-    b = checkcheck_b(arr)
-    w = checkcheck_w(arr)
+    
+    b = checkcheck(arr, ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'])
+    w = checkcheck(arr, ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'])
     if (b >= w):
         return w
     else:
@@ -38,8 +28,8 @@ for i in range(0, n-7):
     for j in range(0, m-7):
         cstr = ""
         for l in c:
-            cstr += str(l[j:j+8])
-        cnt = checking(list(deque(cstr)))
+            cstr += l[j:j+8]
+        cnt = checking(list(cstr))
         if (cnt < min_num):
             min_num = cnt
 print(min_num)
