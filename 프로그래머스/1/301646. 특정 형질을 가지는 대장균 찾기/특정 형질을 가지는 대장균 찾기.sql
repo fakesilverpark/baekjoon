f76f008fd77386ac4b671f0801e6379239f4b1a5
박가은 <167179789+fakesilverpark@users.noreply.github.com>
2024-09-15 21:52:57 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,2 +1,7 @@
+#비트 연산자 활용하기
+/*select count(*) as COUNT from ECOLI_DATA
+where (((GENOTYPE & 2) = 0) and ((GENOTYPE & 1) or (GENOTYPE & 4)));*/
+
+#와일드카드 활용하기
 select count(*) as COUNT from ECOLI_DATA
-where (((GENOTYPE & 2) = 0) and ((GENOTYPE & 1) or (GENOTYPE & 4)));
+where (bin(GENOTYPE) not like "%1_") and ((bin(GENOTYPE) like "%1") or (bin(GENOTYPE) like "%1__"));
