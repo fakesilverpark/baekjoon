bde5f4087e1bf2f552a229974d155a2bbc646d5e
박가은 <167179789+fakesilverpark@users.noreply.github.com>
2025-04-12 23:55:21 +0900

    [Bronze III] Title: 집 주소, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1284. 집 주소/집 주소.py
+++ 백준/Bronze/1284. 집 주소/집 주소.py
@@ -1,7 +1,3 @@
-# 각 숫자 사이에는 1cm의 여백이 들어가야한다.
-# 1은 2cm의 너비를 차지해야한다. 0은 4cm의 너비를 차지해야한다. 나머지 숫자는 모두 3cm의 너비를 차지한다.
-# 호수판의 경계와 숫자 사이에는 1cm의 여백이 들어가야한다.
-
 def f(n):
     if (n == 0):
         return 4
@@ -16,9 +12,7 @@ while (1):
         break
     else:
         leng = 0
-        cnt = 0
         while (num != 0):
-            leng += f(num % 10)
-            cnt += 1
+            leng += f(num % 10) + 1
             num //= 10
-        print(leng + 1 * (cnt + 1))
+        print(leng + 1)
